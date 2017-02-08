3f91773ac46644b8bc3e944ff7219fcd93233264
Elrey Belmonti <elbelmonti@gmail.com>
2017-02-07 17:18:02 -0800

    WIP post method

--- Sinatra/wiki.rb
+++ Sinatra/wiki.rb
@@ -6,6 +6,12 @@ rescue Errno::ENOENT
   return nil
 end
 
+def save_content(title, content)
+	File.open("pages/#{title}.txt", "w") do |file|
+		file.print(content)
+	end
+end
+
 get "/" do 
 	erb :welcome
 end
@@ -18,5 +24,10 @@ get "/:title" do
 	@title = params[:title]
 	@content = page_content(@title)
 	erb :show
+end
+
+#{"title"=>"title", "content"=>"my content stuff\r\n"}
+post "/create" do
+	params.inspect
 end
 
