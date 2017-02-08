f1a77ead1890f74d1af740fa8925968d2ca5601a
Elrey Belmonti <elbelmonti@gmail.com>
2017-02-07 16:40:54 -0800

    Delete test file

--- Sinatra/test.rb
+++
@@ -1,7 +0,0 @@
-def save_content(title, content)
-	File.open("pages/#{title}.txt", "w") do |file|
-		file.print(content)
-	end
-end
-
-save_content("Nick Pettit", "Treehouse teacher and awesome guy.")
