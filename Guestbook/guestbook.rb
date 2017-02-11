dfb6f44ccd46c0c3c5955982b0e7b26e926e82a5
Elrey Belmonti <elbelmonti@gmail.com>
2017-02-10 17:51:45 -0800

    WIP add a signature

--- Guestbook/guestbook.rb
+++ Guestbook/guestbook.rb
@@ -1,6 +1,26 @@
 require 'sinatra'
 
+def save_signature(signature)
+  File.open("signatures.txt", "a") do |file|
+    file.puts signature
+  end
+end
+
 get "/" do 
 	erb :welcome
 end
+
+get "/new" do
+  erb :new
+end
+
+get "signatures/new" do
+	
+end
+
+post "/signatures/create" do 
+	save_signature(params["signature"])
+	redirect "/signatures/new"
+end
+
 
