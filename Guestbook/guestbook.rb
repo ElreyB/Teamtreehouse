8c28e9bb269f8c38f094eae75c91e7d24c52d25a
Elrey Belmonti <elbelmonti@gmail.com>
2017-02-11 09:02:29 -0800

    WIP

--- Guestbook/guestbook.rb
+++ Guestbook/guestbook.rb
@@ -1,9 +1,15 @@
 require 'sinatra'
 
 def save_signature(signature)
-  File.open("signatures.txt", "a") do |file|
+  File.open("pages/signatures.txt", "a") do |file|
     file.puts signature
   end
+end
+
+def save_content(title, content)
+	File.open("pages/#{title}.txt", "w") do |file|
+		file.print(content)
+	end
 end
 
 get "/" do 
@@ -15,7 +21,7 @@ get "/new" do
 end
 
 get "signatures/new" do
-	
+
 end
 
 post "/signatures/create" do 
