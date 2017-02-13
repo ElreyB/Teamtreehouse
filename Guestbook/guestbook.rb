bed528c33ba577568b5132cde51c7002855b48f7
Elrey Belmonti <elbelmonti@gmail.com>
2017-02-13 10:53:16 -0800

    Add an adit form

--- Guestbook/guestbook.rb
+++ Guestbook/guestbook.rb
@@ -6,12 +6,26 @@ def save_signature(signature)
   end
 end
 
+# Loads the signature at the given line index.
+def load_signature(index)
+  lines = File.readlines("signatures.txt")
+  # Parameter is a string; convert to integer
+  index = index.to_i
+  lines[index]
+end
+
 get "/" do 
 	erb :welcome
 end
 
 get "/signatures/new" do
   erb :new
+end
+
+get "/:index/edit" do 
+	@index = params[:index]
+	@signature = load_signature(@index)
+	erb :edit
 end
 
 post "/signatures/create" do 
